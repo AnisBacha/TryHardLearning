7b019311a85e057b748f5bee74e8d952c8c2b1e1
AnisBacha <76214356+AnisBacha@users.noreply.github.com>
2023-09-10 11:48:19 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,11 +13,9 @@ let correctKeysPressed = 0;
 let startTime;
 let endTime;
 let typingIsRunning = false;
+let testShouldEnd = false;
 
-newTestbutton.addEventListener("click", (event) => {
-  window.location.reload();
-});
-
+// ||| GENERATING THE TEXT TO DISPLAY
 const generateRandomWords = async () => {
   const randomWord = await fetch(
     `https://random-word-api.herokuapp.com/word?number=300`
@@ -28,20 +26,19 @@ const generateRandomWords = async () => {
 };
 const generateRandomQuote = async () => {
   const randomQuote = await fetch(
-    `https://api.api-ninjas.com/v1/quotes?category=business`,
-    {
-      method: "GET",
-      url: "https://api.api-ninjas.com/v1/quotes?category=business",
-      headers: { "X-Api-Key": "U6qNWEiNpz2VIhl1LQbI8A==PU9kt7ypB548E4TB" },
-      contentType: "application/json",
+    "https://api.quotable.io/quotes/random?minLength=100",    {
+      method: "GET"
     }
   );
+  // https://api.api-ninjas.com/v1/quotes?category=business
+  //https://github.com/lukePeavey/quotable
   const jsonRandomQuote = await randomQuote.json();
-  return jsonRandomQuote;
+    return jsonRandomQuote;
 };
-generateRandomQuote();
+
 const randomWordsTypingTest = async () => {
   try {
+    selectedTypingTypeStyle(randomWordsSelection , randomQuotesSelection);
     const wordsArray = await generateRandomWords();
     text = wordsArray.join(" ");
 
@@ -54,8 +51,10 @@ const randomWordsTypingTest = async () => {
 };
 const randomQuotesTypingTest = async () => {
   try {
+    selectedTypingTypeStyle(randomQuotesSelection , randomWordsSelection);
     const quote = await generateRandomQuote();
-    text = quote[0].quote;
+    text = quote[0].content;
+    console.log(text)
     paragraphAndInputFunctionality();
     statsFunctionality();
   } catch (error) {
@@ -63,15 +62,28 @@ const randomQuotesTypingTest = async () => {
     alert("Failed to load random Quote. Please try again later.");
   }
 };
+const selectedTypingTypeStyle = (addClass , removeClass) =>{
+  addClass.classList.add("selectedTypingType");
+  removeClass.classList.remove("selectedTypingType");
+}
 
-// Random Words for Typing Test
-randomWordsTypingTest();
+// ||| DISPLAYING THE GENERATED TEXT
+let textType;
+window.addEventListener("load" , (event) =>{
+  textType = JSON.parse(localStorage.getItem("TextType"));
+  console.log(textType);
+  textType === "quotes" ? randomQuotesTypingTest() : randomWordsTypingTest();
+})
+
 randomWordsSelection.addEventListener("click", (event) => {
-  randomWordsTypingTest();
+  textType = "words"
+  localStorage.setItem("TextType" , JSON.stringify(textType));
+  window.location.reload();
 });
 randomQuotesSelection.addEventListener("click", (event) => {
+  textType = "quotes";
+  localStorage.setItem("TextType" , JSON.stringify(textType));
   window.location.reload();
-  randomQuotesTypingTest();
 });
 
 // ||| PARAGRAPH AND INPUT ELEMENTS CUSTOMIZATION
@@ -89,7 +101,7 @@ const paragraphAndInputFunctionality = () => {
   function getLastWordIneachLine() {
     // When we limit the text shown it won't get the last words properly So we need to delay it
     textElement.classList.remove("limitTextShown");
-    // This will make the height of the textElement consist one line
+    // This will make the height of the textElement consist of the height of one line
     textElement.innerHTML = words[0];
     // Here we get the height of one line
     let height = textElement.clientHeight;
@@ -98,6 +110,7 @@ const paragraphAndInputFunctionality = () => {
       textElement.innerHTML = textElement.innerHTML + " " + words[i];
       // while we adding the random words to our element, we check if the
       if (textElement.clientHeight > height) {
+        //Here We Update the height of textElement and we store the last word in the line.
         height = textElement.clientHeight;
         lastWordsNonFiltered[i - 1] = words[i - 1];
         console.log(lastWordsNonFiltered[i - 1]);
@@ -191,6 +204,11 @@ const paragraphAndInputFunctionality = () => {
       if (typedWord === lastWords[lastWordCount]) {
         shouldScrollDown = true;
       }
+      
+    }
+    if(typedWord === words[words.length - 1]) {
+      testShouldEnd = true;
+      console.log(testShouldEnd)
     }
   });
 
@@ -235,22 +253,23 @@ const statsFunctionality = () => {
       timer--;
       timerElement.textContent = timer;
     }
-    endTest(timerInterval, wpmInterval);
+    if(timer === 0 || testShouldEnd){
+      endTest(timerInterval, wpmInterval);
+    }
+    
   };
   const endTest = (timerInterval, wpmInterval) => {
-    if (timer === 0) {
       input.disabled = true;
       console.log("test is ended");
       clearInterval(timerInterval);
       clearInterval(wpmInterval);
       calculateAccuracy();
-    }
+    
   };
   const calculateWpm = () => {
     if (Date.now() >= endTime) {
       return;
     }
-
     if (Date.now() <= endTime) {
       const elapsedSeconds = (Date.now() - startTime) / 1000;
       const rawWPM = Math.round(totalKeysPressed / 5 / (elapsedSeconds / 60));
@@ -265,5 +284,9 @@ const statsFunctionality = () => {
   };
 
   const timerInterval = setInterval(startTest, 1000);
-  const wpmInterval = setInterval(calculateWpm, 500);
+  const wpmInterval = setInterval(calculateWpm, 1500);
 };
+
+newTestbutton.addEventListener("click", (event) => {
+  window.location.reload();
+});
